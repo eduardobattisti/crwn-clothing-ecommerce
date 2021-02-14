a2c68bf93c0685fa825dfb1a4cb435106097247b
eduardobattisti <eduardobattisti@protonmail.com>
2021-02-13 23:25:01 -0300

    adding sign in, sign out, custom button

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInSignOutPage from './pages/sign-in-and-sign-out/sign-in-and-sign-out.component';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signIn' component={SignInSignOutPage} />
       </Switch>
     </div>
   );
