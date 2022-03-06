9fd48d0fa549c862c90b60c9ea4d5eab173df7f5
eduardobattisti <eduardobattisti10@gmail.com>
2022-03-06 15:58:20 -0300

    fix: trying to solve heroku dploy

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
+import path from 'path';
+import {fileURLToPath} from 'url';
 import bodyParser from "body-parser";
 import express from "express";
-import path from "path";
 import dotenv from "dotenv";
 import Stripe from "stripe";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 if (process.env.NODE_ENV !== "production") dotenv.config();
 
