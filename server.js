707d405675c1a74f59db0558467ba9c8a7711358
eduardobattisti <eduardobattisti10@gmail.com>
2022-03-11 19:37:51 -0300

    feat: update manifest and https

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-import path from 'path';
-import {fileURLToPath} from 'url';
+import path from "path";
+import { fileURLToPath } from "url";
 import bodyParser from "body-parser";
 import express from "express";
 import dotenv from "dotenv";
 import Stripe from "stripe";
-import compression from 'compression';
+import compression from "compression";
+import enforce from 'express-sslify';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -18,15 +19,21 @@ const port = process.env.PORT || 5000;
 
 app.use(compression());
 app.use(bodyParser.json());
+app.use(enforce.HTTPS({ trustProtoHeader: true }))
 app.use(express.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === "production") {
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
   app.use(express.static(path.join((__dirname, "client/build"))));
 
   app.get("*", (_req, res) => {
     res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
+
+app.get("/service-worker.js", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "..", "build", "service-worker.js"));
+});
 
 app.post("/payment", (req, res) => {
   const body = {
